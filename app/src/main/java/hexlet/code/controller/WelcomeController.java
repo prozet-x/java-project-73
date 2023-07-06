58d05529b368b2c30b540fbb2e55017a9b790902
Dmitriy Nesterovich <abcd-xyz@list.ru>
2023-07-06 18:11:18 +0700

    st 1

--- app/src/main/java/hexlet/code/controller/WelcomeController.java
+++ app/src/main/java/hexlet/code/controller/WelcomeController.java
@@ -1,12 +1,14 @@
 package hexlet.code.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class WelcomeController {
 
-    @GetMapping("/welcome")
+    @GetMapping("welcome")
     public String welcome() {
         return "Welcome to Spring";
     }

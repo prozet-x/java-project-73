179719f5f51fa45c6b80d2440691f89253b297e5
Dmitriy Nesterovich <abcd-xyz@list.ru>
2023-09-01 18:29:54 +0700

    st 10

--- app/src/main/java/hexlet/code/controller/LabelController.java
+++ app/src/main/java/hexlet/code/controller/LabelController.java
@@ -4,6 +4,11 @@ import hexlet.code.dto.LabelDto;
 import hexlet.code.model.Label;
 import hexlet.code.repository.LabelRepository;
 import hexlet.code.service.LabelService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,17 +27,39 @@ public class LabelController {
     private LabelRepository labelRepository;
     private LabelService labelService;
 
+    @Operation(summary = "Create new label")
+    @ApiResponse(responseCode = "201", description = "New label created")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Label createNew(@RequestBody @Valid final LabelDto labelDto) {
         return labelService.createNew(labelDto);
     }
 
+    @Operation(summary = "Get all labels")
+    @ApiResponse(
+            responseCode = "200",
+            description = "All labels got",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Label.class)))
+            }
+    )
     @GetMapping
     public List<Label> getAll() {
         return labelRepository.findAll();
     }
 
+    @Operation(summary = "Get label by id")
+    @ApiResponse(responseCode = "200",
+            description = "Label got",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Label.class)
+                    )
+            }
+    )
     @GetMapping(ID)
     public Label getById(@PathVariable final Long id) {
         return labelRepository.findById(id).get();
